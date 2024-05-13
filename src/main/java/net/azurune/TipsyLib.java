04152d1a7a1c97d72b557b56602eecafe286a2fe
yirmiri <yirmirilucy@gmail.com>
2024-05-12 23:54:42 -0400

    Initial commit

---
+++ src/main/java/net/azurune/TipsyLib.java
@@ -0,0 +1,14 @@
+package net.azurune;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TipsyLib implements ModInitializer {
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
