ee9d0109567c9ebed60756e54fa3e6f1c9be0f2f
yirmiri <yirmirilucy@gmail.com>
2024-06-13 09:31:10 -0400

    Bandage solutions before 1.21

--- common/src/main/java/net/azurune/tipsylib/core/mixin/GuiMixin.java
+++ common/src/main/java/net/azurune/tipsylib/core/mixin/GuiMixin.java
@@ -1,6 +1,5 @@
 package net.azurune.tipsylib.core.mixin;
 
-import com.mojang.authlib.minecraft.client.MinecraftClient;
 import net.azurune.tipsylib.TipsyLibConstants;
 import net.azurune.tipsylib.core.register.TLStatusEffects;
 import net.minecraft.client.Minecraft;
@@ -21,21 +20,7 @@ import static java.util.Map.entry;
 
 @Mixin(Gui.class)
 public abstract class GuiMixin {
-
-    @Unique
-    private static final Map<MobEffect, ResourceLocation> CONTAINER_ICONS_MAP = Map.ofEntries(
-            entry(TLStatusEffects.CONFUSION, new ResourceLocation(TipsyLibConstants.MOD_ID, "textures/gui/heart_container/confusion_container.png")),
-            entry(TLStatusEffects.BERSERK, new ResourceLocation(TipsyLibConstants.MOD_ID, "textures/gui/heart_container/berserk_container.png")),
-            entry(TLStatusEffects.SHATTERSPLEEN, new ResourceLocation(TipsyLibConstants.MOD_ID, "textures/gui/heart_container/transparent_container.png"))
-    );
-
-    @Unique
-    private static final Map<MobEffect, ResourceLocation> BLINKING_CONTAINER_ICONS_MAP = Map.ofEntries(
-            entry(TLStatusEffects.CONFUSION, new ResourceLocation(TipsyLibConstants.MOD_ID, "textures/gui/heart_blink/confusion_blink.png")),
-            entry(TLStatusEffects.BERSERK, new ResourceLocation(TipsyLibConstants.MOD_ID, "textures/gui/heart_blink/berserk_blink.png")),
-            entry(TLStatusEffects.SHATTERSPLEEN, new ResourceLocation(TipsyLibConstants.MOD_ID, "textures/gui/heart_blink/transparent_blink.png"))
-    );
-
+//TODO: Add berserk heart icons
     @Unique
     private static final Map<MobEffect, ResourceLocation> OVERIDE_HEART_ICONS_MAP = Map.ofEntries(
             entry(TLStatusEffects.CONFUSION, new ResourceLocation(TipsyLibConstants.MOD_ID, "textures/gui/confused_hearts.png"))
@@ -44,17 +29,43 @@ public abstract class GuiMixin {
     @Unique
     private static final Map<MobEffect, ResourceLocation> HEART_ICONS_MAP = Map.ofEntries(
             entry(TLStatusEffects.CONFUSION, new ResourceLocation(TipsyLibConstants.MOD_ID, "textures/gui/confused_hearts.png")),
+            //entry(TLStatusEffects.BERSERK, new ResourceLocation(TipsyLibConstants.MOD_ID, "textures/gui/berserk_hearts.png")),
             entry(TLStatusEffects.HEMOLACRIA, new ResourceLocation(TipsyLibConstants.MOD_ID, "textures/gui/hemolacria_hearts.png")),
             entry(TLStatusEffects.VENOM, new ResourceLocation(TipsyLibConstants.MOD_ID, "textures/gui/venom_hearts.png")),
             entry(TLStatusEffects.INTERNAL_BLEEDING, new ResourceLocation(TipsyLibConstants.MOD_ID, "textures/gui/bleeding_hearts.png")),
             entry(TLStatusEffects.SHATTERSPLEEN, new ResourceLocation(TipsyLibConstants.MOD_ID, "textures/gui/shatterspleen_hearts.png")),
-            entry(TLStatusEffects.BERSERK, new ResourceLocation(TipsyLibConstants.MOD_ID, "textures/gui/berserk_hearts.png")),
             entry(TLStatusEffects.DEVOUR, new ResourceLocation(TipsyLibConstants.MOD_ID, "textures/gui/devour_hearts.png"))
+    );
+
+    @Unique
+    private static final Map<MobEffect, ResourceLocation> CONTAINER_ICONS_MAP = Map.ofEntries(
+            entry(TLStatusEffects.CONFUSION, new ResourceLocation(TipsyLibConstants.MOD_ID, "textures/gui/heart_container/confusion_container.png")),
+            //entry(TLStatusEffects.BERSERK, new ResourceLocation(TipsyLibConstants.MOD_ID, "textures/gui/heart_container/berserk_container.png")),
+            entry(TLStatusEffects.SHATTERSPLEEN, new ResourceLocation(TipsyLibConstants.MOD_ID, "textures/gui/heart_container/transparent_container.png"))
+    );
+
+    @Unique
+    private static final Map<MobEffect, ResourceLocation> BLINKING_CONTAINER_ICONS_MAP = Map.ofEntries(
+            entry(TLStatusEffects.CONFUSION, new ResourceLocation(TipsyLibConstants.MOD_ID, "textures/gui/heart_blink/confusion_blink.png")),
+            //entry(TLStatusEffects.BERSERK, new ResourceLocation(TipsyLibConstants.MOD_ID, "textures/gui/heart_blink/berserk_blink.png")),
+            entry(TLStatusEffects.SHATTERSPLEEN, new ResourceLocation(TipsyLibConstants.MOD_ID, "textures/gui/heart_blink/transparent_blink.png"))
     );
 
     @Inject(method = "renderHeart", at = @At("HEAD"), cancellable = true)
     private void tipsylib_renderHeart(GuiGraphics ctx, Gui.HeartType type, int x, int y, int v, boolean blinking, boolean halfHeart, CallbackInfo ci) {
         ResourceLocation resourceLocation;
+        if (!blinking && type == Gui.HeartType.NORMAL && Minecraft.getInstance().cameraEntity instanceof Player player) {
+            for (MobEffect effect : HEART_ICONS_MAP.keySet()) {
+                if (player.hasEffect(effect)) {
+                    resourceLocation = HEART_ICONS_MAP.get(effect);
+                    ctx.blit(resourceLocation, x, y, halfHeart ? 9 : 0, v, 9, 9);
+
+                    ci.cancel();
+                    return;
+                }
+            }
+        }
+
         if (!blinking && type == Gui.HeartType.CONTAINER && Minecraft.getInstance().cameraEntity instanceof Player player) {
 
             for (MobEffect effect : CONTAINER_ICONS_MAP.keySet()) {
@@ -72,18 +83,6 @@ public abstract class GuiMixin {
             for (MobEffect effect : BLINKING_CONTAINER_ICONS_MAP.keySet()) {
                 if (player.hasEffect(effect)) {
                     resourceLocation = BLINKING_CONTAINER_ICONS_MAP.get(effect);
-                    ctx.blit(resourceLocation, x, y, halfHeart ? 9 : 0, v, 9, 9);
-
-                    ci.cancel();
-                    return;
-                }
-            }
-        }
-
-        if (!blinking && type == Gui.HeartType.NORMAL && Minecraft.getInstance().cameraEntity instanceof Player player) {
-            for (MobEffect effect : HEART_ICONS_MAP.keySet()) {
-                if (player.hasEffect(effect)) {
-                    resourceLocation = HEART_ICONS_MAP.get(effect);
                     ctx.blit(resourceLocation, x, y, halfHeart ? 9 : 0, v, 9, 9);
 
                     ci.cancel();
--- common/src/main/java/net/azurune/tipsylib/core/register/TLStatusEffects.java
+++ common/src/main/java/net/azurune/tipsylib/core/register/TLStatusEffects.java
@@ -55,7 +55,7 @@ public class TLStatusEffects {
     public static final MobEffect INTERNAL_BLEEDING = new NoSpecialEffect(MobEffectCategory.HARMFUL, 0x410909);
     public static final MobEffect CAFFEINE_CRASH = new NoSpecialEffect(MobEffectCategory.HARMFUL, 0x313d99);
     public static final MobEffect SMOULDERING = new SmoulderingEffect(MobEffectCategory.HARMFUL, 0xdf9d34);
-    public static final MobEffect FROSTBITE = new FrostbiteEffect(MobEffectCategory.HARMFUL, 0x92f3f6);
+    //public static final MobEffect FROSTBITE = new FrostbiteEffect(MobEffectCategory.HARMFUL, 0x92f3f6); //TODO: Smouldering effect but for freezing
 
     public static void loadEffects() {
         //BENEFICIAL
@@ -102,7 +102,7 @@ public class TLStatusEffects {
         registerEffect("internal_bleeding", () -> INTERNAL_BLEEDING);
         registerEffect("caffeine_crash", () -> CAFFEINE_CRASH.addAttributeModifier(Attributes.ATTACK_DAMAGE, "0a921b76-10d3-4038-8a2d-7e53ad32ef3d", -1.0, AttributeModifier.Operation.ADDITION).addAttributeModifier(Attributes.MOVEMENT_SPEED, "3ecec3d4-8bad-4f10-b870-83228e444672", -0.03, AttributeModifier.Operation.ADDITION).addAttributeModifier(Attributes.ARMOR, "bb33d1c3-68b1-4413-958e-3a6b32e991be", -2.0, AttributeModifier.Operation.ADDITION).addAttributeModifier(Attributes.MAX_HEALTH, "659863ac-6cc0-4f4e-92c4-96fc04df37bf", -2.0, AttributeModifier.Operation.ADDITION).addAttributeModifier(Attributes.JUMP_STRENGTH, "c428b22a-8db3-4778-9dba-27fae9f9b6a4", -1.0, AttributeModifier.Operation.ADDITION).addAttributeModifier(Attributes.LUCK, "5213feef-1d5f-407a-a708-629b79d12bf3", -1.0, AttributeModifier.Operation.ADDITION));
         registerEffect("smouldering", () -> SMOULDERING);
-        registerEffect("frostbite", () -> FROSTBITE);
+        //registerEffect("frostbite", () -> FROSTBITE); //TODO: FINISH
     }
 
     //TODO: Caffeinated should grant caffeine crash when ending
