2916dff9f3a0c084478ae8a6e060ee5b54fc6ea3
yirmiri <yirmirilucy@gmail.com>
2024-06-23 14:55:33 -0400

    Finish

--- common/src/main/java/net/azurune/tipsylib/core/mixin/PlayerSkinMixin.java
+++ common/src/main/java/net/azurune/tipsylib/core/mixin/PlayerSkinMixin.java
@@ -1,17 +1,16 @@
 package net.azurune.tipsylib.core.mixin;
 
-import com.mojang.blaze3d.vertex.PoseStack;
 import net.azurune.tipsylib.TipsyLib;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.player.AbstractClientPlayer;
-import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.resources.PlayerSkin;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
@@ -19,6 +18,7 @@ import java.util.List;
 
 @Mixin(PlayerSkin.class)
 public class PlayerSkinMixin {
+    @Mutable @Shadow @Final private ResourceLocation texture;
     //Capes for devs and contributors of any Azurune mods
     private static final List<String> AZURUNE = List.of("1cedf927-5c8f-4650-95e9-808fc8f94d00", "bd35c402-fa9c-4d00-afe6-b4ed9ebe90c4"); //Yirmiri
     private static final List<String> REDEYEVAIN = List.of("2913d971-a58d-4566-8706-b4fb5eacb954"); //Nutria
@@ -41,49 +41,49 @@ public class PlayerSkinMixin {
         Player player = Minecraft.getInstance().player;
         String stringUUID = player.getStringUUID();
         if (AZURUNE.contains(stringUUID)) {
-            ResourceLocation texture = TipsyLib.id("textures/capes/azurune.png");
+            texture = TipsyLib.id("textures/capes/azurune.png");
         }
         if (REDEYEVAIN.contains(stringUUID)) {
-            ResourceLocation texture = TipsyLib.id("textures/capes/redeyevain.png");
+            texture = TipsyLib.id("textures/capes/redeyevain.png");
         }
         if (BEETROOT.contains(stringUUID)) {
-            ResourceLocation texture = TipsyLib.id("textures/capes/beetroot.png");
+            texture = TipsyLib.id("textures/capes/beetroot.png");
         }
         if (DEMONHEART.contains(stringUUID)) {
-            ResourceLocation texture = TipsyLib.id("textures/capes/demonheart.png");
+            texture = TipsyLib.id("textures/capes/demonheart.png");
         }
         if (ACCURSED.contains(stringUUID)) {
-            ResourceLocation texture = TipsyLib.id("textures/capes/accursed.png");
+            texture = TipsyLib.id("textures/capes/accursed.png");
         }
         if (HEX.contains(stringUUID)) {
-            ResourceLocation texture = TipsyLib.id("textures/capes/hex.png");
+            texture = TipsyLib.id("textures/capes/hex.png");
         }
         if (PUMPKIN_QUEEN.contains(stringUUID)) {
-            ResourceLocation texture = TipsyLib.id("textures/capes/pumpkin_queen.png");
+            texture = TipsyLib.id("textures/capes/pumpkin_queen.png");
         }
         if (SAD_CLOUD.contains(stringUUID)) {
-            ResourceLocation texture = TipsyLib.id("textures/capes/sad_cloud.png");
+            texture = TipsyLib.id("textures/capes/sad_cloud.png");
         }
         if (WRATHFUL.contains(stringUUID)) {
-            ResourceLocation texture = TipsyLib.id("textures/capes/wrathful.png");
+            texture = TipsyLib.id("textures/capes/wrathful.png");
         }
         if (BUG.contains(stringUUID)) {
-            ResourceLocation texture = TipsyLib.id("textures/capes/bug.png");
+            texture = TipsyLib.id("textures/capes/bug.png");
         }
         if (AXOLOTL.contains(stringUUID)) {
-            ResourceLocation texture = TipsyLib.id("textures/capes/axolotl.png");
+            texture = TipsyLib.id("textures/capes/axolotl.png");
         }
         if (LIGHTNING.contains(stringUUID)) {
-            ResourceLocation texture = TipsyLib.id("textures/capes/lightning.png");
+            texture = TipsyLib.id("textures/capes/lightning.png");
         }
         if (REBELLIOUS.contains(stringUUID)) {
-            ResourceLocation texture = TipsyLib.id("textures/capes/rebellious.png");
+            texture = TipsyLib.id("textures/capes/rebellious.png");
         }
         if (DICE.contains(stringUUID)) {
-            ResourceLocation texture = TipsyLib.id("textures/capes/dice.png");
+            texture = TipsyLib.id("textures/capes/dice.png");
         }
         if (FUDGE_SUNDAE.contains(stringUUID)) {
-            ResourceLocation texture = TipsyLib.id("textures/capes/fudge_sundae.png");
+            texture = TipsyLib.id("textures/capes/fudge_sundae.png");
         }
     }
 }
