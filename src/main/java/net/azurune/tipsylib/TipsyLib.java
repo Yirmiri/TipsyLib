5fdd7cce20558267c3053a3239285145a33dcf2b
yirmiri <yirmirilucy@gmail.com>
2024-06-23 17:27:58 -0400

    Back to square one

---
+++ src/main/java/net/azurune/tipsylib/TipsyLib.java
@@ -0,0 +1,17 @@
+package net.azurune.tipsylib;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TipsyLib implements ModInitializer {
+	public static final String MOD_ID = "tipsylib";
+	public static final String MOD_NAME = "TipsyLib";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
