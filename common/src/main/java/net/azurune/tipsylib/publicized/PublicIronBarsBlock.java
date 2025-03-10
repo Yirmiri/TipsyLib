e3ebbc53165c62c8b2f0df516ef854b36cf8d64d
yirmiri <yirmirilucy@gmail.com>
2025-03-10 15:11:45 -0400

    Ready for release !?

---
+++ common/src/main/java/net/azurune/tipsylib/publicized/PublicIronBarsBlock.java
@@ -0,0 +1,9 @@
+package net.azurune.tipsylib.publicized;
+
+import net.minecraft.world.level.block.IronBarsBlock;
+
+public class PublicIronBarsBlock extends IronBarsBlock {
+    public PublicIronBarsBlock(Properties properties) {
+        super(properties);
+    }
+}
