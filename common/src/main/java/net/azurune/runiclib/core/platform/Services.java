c8ab065493d33d029c91fc9b243190068d30c6d0
Yirmiri <68178497+Yirmiri@users.noreply.github.com>
2025-09-09 21:58:25 -0400

    Planned Refactor

--- common/src/main/java/net/azurune/runiclib/core/platform/Services.java
+++ common/src/main/java/net/azurune/runiclib/core/platform/Services.java
@@ -7,6 +7,10 @@ import net.azurune.runiclib.core.platform.services.RLRegistryHelper;
 
 import java.util.ServiceLoader;
 
+/**
+ * Services will be refactored to RLServices for versions of this mod on Minecraft version 1.22+
+ * This change is to help differentiate between this class and other classes with similar names
+ */
 public class Services {
     public static final RLPlatformHelper PLATFORM = load(RLPlatformHelper.class);
     public static final RLRegistryHelper REGISTRY = load(RLRegistryHelper.class);
